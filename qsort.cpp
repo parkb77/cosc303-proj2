2c62417a4c84c7f3c4f901d4e04f8724949fa412
Casey <chiggi18@vols.utk.edu>
2026-09-14 14:49:07 -0400

    got string and int comparison functions almost done next is to run qsort

--- qsort.cpp
+++ qsort.cpp
@@ -6,7 +6,25 @@
 #include <array>
 #include <iostream>
 
+
+int compareStringP(const void * a, const void * b){
+	return strcmp(*(const char **) a, *(const char **) b);
+}
+
+int compareIntP(const void * a, const void * b){
+	const Node * ia = *(const Node **) a;
+	const Node * ib = *(const Node **) b;
+
+	if (ia->number < ib->number) return -1;  // sorts in ascending order
+        if (ia->number > ib->number) return 1;
+        return 0;
+}
+
+
 void qsort_sort(List &l, bool numeric) {
+	array<int,l.size> numbers;
+
+
 
 }
 
