0302806f6ac43f8b7a08fd732f2deedc550917fd
Casey <chiggi18@vols.utk.edu>
2026-09-14 13:35:56 -0400

    added pushfront to list.cpp

--- list.cpp
+++ list.cpp
@@ -13,3 +13,10 @@ List::~List() {
 void List::push_front(const std::string &s) {
  
 }
+
+// Push a value to the front of the list
+void List::push_front(const std::string &s) {
+    Node *new_node = newNode(s);
+    new_node->next = head;
+    head = new_node;
+}
