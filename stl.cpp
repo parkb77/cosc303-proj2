96cd1f8beef3d8059a2df434963eefb7aab50b08
parkb77 <parkerbabb7@gmail.com>
2026-09-14 15:26:05 -0400

    Update stl.cpp

--- stl.cpp
+++ stl.cpp
@@ -6,5 +6,41 @@
 #include <iostream>
 
 void stl_sort(List &l, bool numeric) {
+	vector <Node*> s;
+
+	//Test for empty list
+	if (l.head == nullptr) { 
+		return
+	}
+
+	//Fill Node vector 's'
+	Node *curr = l.head; 
+	while (*curr != nullptr) { 
+	s.push_back(curr); 
+	curr = curr->next;
+	}
+
+	if (numeric) { 
+		sort(s.begin(), s.end(); node_number_compare);
+	} else {
+		sort(s.begin(), s.end(); node_string_compare);
+	}
+	
+
+	//Order the node pointers based on sorted order
+	for (int i = 0; i < s.size(); i++) { 
+		s[i]->next = s[i + 1]; 
+	} 
+
+	//Set new list head
+	l.head = s[0];
 }
 
+// a < b to compare in ascending order (source: google)  
+bool node_number_compare(const Node *a, const Node *b) {
+	return a->number < b->number;
+}
+
+bool node_string_compare(const Node *a, const Node *b) {
+	return a->string < b->string;
+}
