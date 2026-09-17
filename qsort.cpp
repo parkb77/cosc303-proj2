0eccb141a8f85aeed66304b9d25fd4df8e151e46
Casey-h-vols <chiggi18@vols.utk.edu>
2026-09-16 21:01:34 -0400

    Update qsort.cpp

--- qsort.cpp
+++ qsort.cpp
@@ -7,14 +7,14 @@
 #include <iostream>
 
 
-int compareStringP(const void * a, const void * b){
+int compareStringP(const void * a, const void * b){	// referenced string compare from notes
 	const Node * ia = *(const Node **) a;
         const Node * ib = *(const Node **) b;
 
 	return ia->string.compare(ib->string);
 }
 
-int compareIntP(const void * a, const void * b){
+int compareIntP(const void * a, const void * b){ // referenced int compare from notes
 	const Node * ia = *(const Node **) a;
 	const Node * ib = *(const Node **) b;
 
@@ -26,17 +26,18 @@ int compareIntP(const void * a, const void * b){
 
 void qsort_sort(List &l, bool numeric) {
 	if (l.head == nullptr) return; 
-
+	// init current holding vars and array
 	Node* current = l.head;
 	Node** arr = new Node*[l.size];
 	int index = 0;
-	
+
+	// write node pointers to arr
 	while (current != nullptr){
                 arr[index] = current;
 		index++;
         	current=current->next;
         }
-
+	// run qsort 
 	if (numeric){
 		qsort(arr, l.size, sizeof(Node*), compareIntP);
 	} else {
@@ -59,11 +60,11 @@ void qsort_sort(List &l, bool numeric) {
 	
 	// set new head
 	l.head = arr[0];
-
+	// order underlying linked list
 	for (size_t i = 0; i < l.size -1; i++){
 		arr[i]->next = arr[i+1];
 	}
-
+	// set end to null
 	arr[l.size-1]->next = nullptr;
 
 	delete[] arr;
